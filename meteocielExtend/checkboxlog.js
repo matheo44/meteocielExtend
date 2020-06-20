5a7bcde2b25370eaaa6b9801c51e831977ba5c8e
matheo44 <64661992+matheo44@users.noreply.github.com>
2020-06-20 18:33:56 +0200

    Add files via upload

---
+++ meteocielExtend/checkboxlog.js
@@ -0,0 +1,110 @@
+
+// Affichage de la demande de confirmation d'inscription
+document.getElementById("observations").addEventListener("change", function (e) {
+    console.log("Observations : " + e.target.checked);
+});
+
+document.getElementById("detailsobs").addEventListener("change", function (e) {
+    console.log("détails des obs : " + e.target.checked);
+});
+
+document.getElementById("galerieobs").addEventListener("change", function (e) {
+    console.log("galerie des obs : " + e.target.checked);
+});
+
+document.getElementById("cartePhotos").addEventListener("change", function (e) {
+    console.log("carte des photos : " + e.target.checked);
+});
+
+document.getElementById("température").addEventListener("change", function (e) {
+    console.log("température : " + e.target.checked);
+});
+
+document.getElementById("tempmin").addEventListener("change", function (e) {
+    console.log("température min : " + e.target.checked);
+});
+
+document.getElementById("tempmax").addEventListener("change", function (e) {
+    console.log("température max : " + e.target.checked);
+});
+
+document.getElementById("tempmer").addEventListener("change", function (e) {
+    console.log("température des mers : " + e.target.checked);
+});
+
+document.getElementById("pointrosée").addEventListener("change", function (e) {
+    console.log("point de rosée : " + e.target.checked);
+});
+
+document.getElementById("windchill").addEventListener("change", function (e) {
+    console.log("windchill : " + e.target.checked);
+});
+
+document.getElementById("humidex").addEventListener("change", function (e) {
+    console.log("humidex : " + e.target.checked);
+});
+
+document.getElementById("isobares").addEventListener("change", function (e) {
+    console.log("isobares : " + e.target.checked);
+});
+
+document.getElementById("pression").addEventListener("change", function (e) {
+    console.log("pression : " + e.target.checked);
+});
+
+document.getElementById("ventmoyen").addEventListener("change", function (e) {
+    console.log("vent moyen : " + e.target.checked);
+});
+
+document.getElementById("ventrafales").addEventListener("change", function (e) {
+    console.log("vent rafales : " + e.target.checked);
+});
+
+document.getElementById("nebulosite").addEventListener("change", function (e) {
+    console.log("nébulosité : " + e.target.checked);
+});
+
+document.getElementById("humidite").addEventListener("change", function (e) {
+    console.log("humidité : " + e.target.checked);
+});
+
+document.getElementById("pluie24h").addEventListener("change", function (e) {
+    console.log("pluie en 24 h : " + e.target.checked);
+});
+
+document.getElementById("ensoleillement").addEventListener("change", function (e) {
+    console.log("ensoleillement : " + e.target.checked);
+});
+
+document.getElementById("sondObsAlt").addEventListener("change", function (e) {
+    console.log("saonde obc altitiude : " + e.target.checked);
+});
+
+document.getElementById("satellite").addEventListener("change", function (e) {
+    console.log("satellite : " + e.target.checked);
+});
+
+document.getElementById("impactsfoudre").addEventListener("change", function (e) {
+    console.log("impacts de foudre : " + e.target.checked);
+});
+
+document.getElementById("impactsdirect").addEventListener("change", function (e) {
+    console.log("impacts en direct : " + e.target.checked);
+});
+
+document.getElementById("radarhdzoom").addEventListener("change", function (e) {
+    console.log("radar HD zoom : " + e.target.checked);
+});
+
+document.getElementById("precipitation").addEventListener("change", function (e) {
+    console.log("radar précipitation : " + e.target.checked);
+});
+
+document.getElementById("pluie_neige").addEventListener("change", function (e) {
+    console.log("radar pluie/neige : " + e.target.checked);
+});
+
+document.getElementById("ensoleillement").addEventListener("change", function (e) {
+    console.log("ensoleillement : " + e.target.checked);
+});
+
